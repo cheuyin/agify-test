11bfea69972d59eff01ea6f437685baff21f23f2
Yin <yinstanleycheung@gmail.com>
2022-06-26 11:29:01 -0700

    Create barebones fetch request

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 const nameInput = document.querySelector("input#name");
 const sendButton = document.querySelector("button");
 
+const apiURL = "https://api.agify.io?";
+
+sendButton.addEventListener("click", handleButton)
+
+function handleButton() {
+  const name = nameInput.value;
+  const fetchPromise = fetch(apiURL + "name=" + name);
+  fetchPromise
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      console.log(data);
+    });   
+}
